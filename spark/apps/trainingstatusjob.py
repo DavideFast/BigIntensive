dc3f4e8cd8acf0f3a37fe088354b6b1bd20741ed
DavideFast <davidefast99@gmail.com>
2026-07-02 23:13:38 +0200

    Updates

--- spark/apps/trainingstatusjob.py
+++ spark/apps/trainingstatusjob.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split, lower, col
 
-spark = SparkSession.builder.appName("wordcount-example").getOrCreate()
+spark = SparkSession.builder.appName("training-status-job").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
 
 input_path = "/opt/spark-data/input.txt"
 
