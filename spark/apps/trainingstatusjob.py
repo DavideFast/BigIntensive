0920c18f2cf47608bad0948c2638e84286d75a95
DavideFast <davidefast99@gmail.com>
2026-07-03 00:09:11 +0200

    Update trainingstatusjob.py

--- spark/apps/trainingstatusjob.py
+++ spark/apps/trainingstatusjob.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import  col
 from pyspark.sql.types import StructType, StructField, StringType, DateType
-from pyspark.sql.functions import from_json, max, avg
+from pyspark.sql.functions import from_json, max, avg, datediff, current_date
 
 spark = SparkSession.builder.appName("training-status-job").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
 
@@ -9,7 +9,7 @@ df_kafka = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "l
 
 schema_json = StructType ([
     StructField("Atleta", StringType(), True),
-    StructField("Data", DateType(), True),
+    StructField("Giorno", DateType(), True),
     StructField("Frequenza cardiaca", StringType(), True),
     StructField("Velocità", StringType(), True),
     StructField("Distanza", StringType(), True),
@@ -20,15 +20,23 @@ df_kafka_schema = df_kafka.select(from_json(col("value").cast("string"), schema_
 
 
 
-df_raggruppato = df_kafka_schema.groupBy("id","giorno").agg(max("durata"),avg("velocità"),avg("frequenza cardiaca"),max("distanza"),avg("temperatura"),max("giorno"))
+df_raggruppato = df_kafka_schema.groupBy("atleta","giorno").agg(
+    max("durata").alias("max_durata"),
+    avg("velocità").alias("media_velocita"),
+    avg("frequenza cardiaca").alias("media_freq_cardiaca"),
+    max("distanza").alias("max_distanza"),
+    avg("temperatura").alias("temperatura")
+)
 
 df_risultato = df_raggruppato.withColumn(
     "indice_allenamento",
     col("media_freq_cardiaca") / col("max_durata") / col("media_velocita") / col("temperatura")  # <-- la tua formula
 )
-df_secondo_round = df_risultato.groupBy("id").filter(col("durata")>500 & max(giorno) - col("giorno")<=42).agg(avg("indice_allenamento").alias("indice_allenamento_max"))
+df_secondo_round = df_risultato.filter(
+    (col("max_durata") > 500) & (datediff(current_date(), col("giorno")) <= 42)
+).groupBy("atleta").agg(avg("indice_allenamento").alias("indice_allenamento_max"))
 
-query = df_risultato.writeStream \
+query = df_secondo_round.writeStream \
     .outputMode("complete") \
     .format("console") \
     .start()
