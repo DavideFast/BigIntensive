58351c908422702b4cf81d66c2655027cd401b9c
DavideFast <davidefast99@gmail.com>
2026-07-03 12:29:42 +0200

    Update matricecorrelazione.py

--- spark/apps/matricecorrelazione.py
+++ spark/apps/matricecorrelazione.py
@@ -1,19 +1,16 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, max, avg, datediff, current
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.stat import Correlation
+from pyspark.sql.functions import col, from_json, max, avg, datediff, current, StructType, StructField, StringType, DateType, DoubleType
 
-spark = sparkSession.builder.appName("matrice-correlazione-job").config("spark.jar.packages", "org.postgresql:postgresql:42.7.2").getOrCreate()
+spark = SparkSession.builder.appName("matrice-correlazione-job").config("spark.jar.packages", "org.postgresql:postgresql:42.7.2").getOrCreate()
 
-df_kafka = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","matrice-correlazione").option("startingOffsets","latest").load()
 
+#Prendo tutti gli id degli esercizi da citus e creo uno schema dinamico per il json
 citus_url = "jdbc:postgresql://localhost:5432/bigintensive"
 citus_properties = {"user": "postgres", "password": "postgres", "driver": "org.postgresql.Driver"}
 citus_tabella = spark.read.jdbc(url=citus_url, table = "esercizi", properties=citus_properties)
 
 campi = []
-for field in df_kafka.schema.fields:
-    if field.name != "value":
-        campi.append(field.name)
+for field in citus_tabella.schema.fields:
+        campi.append(StructField(field.id, StringType(),True))
 
 schema_json = StructType(campi)
