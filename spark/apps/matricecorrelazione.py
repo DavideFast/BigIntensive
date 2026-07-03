66052162c0f7ce70f59cdd0fba5c75e4f0d5f53c
DavideFast <davidefast99@gmail.com>
2026-07-03 11:38:31 +0200

    Create matricecorrelazione.py

---
+++ spark/apps/matricecorrelazione.py
@@ -0,0 +1,15 @@
+
+
+spark = sparkSession.builder.appName("matrice-correlazione-job").config("spark.jar.packages", "org.apache.spark:spark-sql_2.12:5.3.0").getOrCreate()
+
+df_kafka = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","matrice-correlazione").option("startingOffsets","latest").load()
+
+schema_json = StructType([
+    StructField("atleta", StringType(), True),
+    StructField("giorno", DateType(), True),
+    StructField("frequenza cardiaca", DoubleType(), True),
+    StructField("velocità", DoubleType(), True),
+    StructField("distanza", DoubleType(), True),
+    StructField("durata", DoubleType(), True),
+    StructField("temperatura", DoubleType(), True)
+])
