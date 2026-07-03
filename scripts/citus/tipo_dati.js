fd7942eb7d128be6d0c2aa55b496c684bcb47f68
DavideFast <davidefast99@gmail.com>
2026-07-03 22:40:58 +0200

    Create tipo_dati.js

---
+++ scripts/citus/tipo_dati.js
@@ -0,0 +1,18 @@
+const struttura_esercizio = {
+  esercizio_id: 1,
+  nome_esercizio: "Panca piana",
+  descrizione: "Esercizio per il petto",
+  serie: 3,
+  ripetizioni: 10,
+  tempo_riposo: "60s",
+};
+
+const struttura_utente = {
+  utente_id: 1,
+  nome: "Mario",
+  cognome: "Rossi",
+  eta: 30,
+  sesso: "M",
+  altezza_cm: 180,
+  peso_kg: 75,
+};
