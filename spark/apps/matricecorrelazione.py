57268d22a50a8ac6dd02190bbfb1449ad9b3e365
DavideFast <davidefast99@gmail.com>
2026-07-03 12:03:43 +0200

    Update matricecorrelazione.py

--- spark/apps/matricecorrelazione.py
+++ spark/apps/matricecorrelazione.py
@@ -3,16 +3,16 @@ from pyspark.sql.functions import col, from_json, max, avg, datediff, current
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.stat import Correlation
 
-spark = sparkSession.builder.appName("matrice-correlazione-job").config("spark.jar.packages", "org.apache.spark:spark-sql_2.12:5.3.0").getOrCreate()
+spark = sparkSession.builder.appName("matrice-correlazione-job").config("spark.jar.packages", "org.postgresql:postgresql:42.7.2").getOrCreate()
 
 df_kafka = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe","matrice-correlazione").option("startingOffsets","latest").load()
 
-schema_json = StructType([
-    StructField("atleta", StringType(), True),
-    StructField("giorno", DateType(), True),
-    StructField("frequenza cardiaca", DoubleType(), True),
-    StructField("velocità", DoubleType(), True),
-    StructField("distanza", DoubleType(), True),
-    StructField("durata", DoubleType(), True),
-    StructField("temperatura", DoubleType(), True)
-])
+citus_url = "jdbc:postgresql://localhost:5432/bigintensive"
+properties = {"user": "postgres", "password": "postgres", "driver": "org.postgresql.Driver"}
+
+campi = []
+for field in df_kafka.schema.fields:
+    if field.name != "value":
+        campi.append(field.name)
+
+schema_json = StructType(campi)
